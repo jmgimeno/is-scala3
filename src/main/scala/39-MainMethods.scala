afe44b0e58e367fbe43cb25fb3d568cd5a43556a
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2026-02-05 08:21:27 +0100

    Update main methods examples and upgrade munit dependency to version 1.2.2

--- src/main/scala/39-MainMethods.scala
+++ src/main/scala/39-MainMethods.scala
@@ -3,20 +3,27 @@
 // Use scala-cli or scala command to run this file
 // scala 39-MainMethods.scala
 
+// To show all main methods in the project, use the following command in sbt
+// sbt show discoverMainClasses
+
 // scala run 39-MainMethods.scala --main-class helloWorld
+// sbt "runMain helloWorld"
 @main def helloWorld(): Unit =
   println("Hello, world")
 
 // Command-line parameters
 
 // scala run 39-MainMethods.scala --main-class helloWorld2 -- patata
+// sbt "runMain helloWorld2 patata"
 @main def helloWorld2(name: String): Unit =
   println(s"Hello, $name")
 
 // scala run 39-MainMethods.scala --main-class helloWorld3 -- patata 42
+// sbt "runMain helloWorld3 patata 42"
 @main def helloWorld3(name: String, age: Int): Unit =
   println(s"Hello, $name, who is $age years old")
 
 // scala run 39-MainMethods.scala --main-class helloWorld4 -- patata poma pera
+// sbt "runMain helloWorld4 patata poma pera"
 @main def helloWorld4(names: String*): Unit =
   names.foreach(name => println(s"Hello, $name"))
